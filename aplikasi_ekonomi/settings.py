1847dcb9726f69895d968b51475a8769e8664a4d
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2022-11-30 10:54:43 +0700

    ganti ke sqlite

--- aplikasi_ekonomi/settings.py
+++ aplikasi_ekonomi/settings.py
@@ -81,13 +81,19 @@ WSGI_APPLICATION = 'aplikasi_ekonomi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ekonomi',
+#         'USER': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ekonomi',
-        'USER': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
